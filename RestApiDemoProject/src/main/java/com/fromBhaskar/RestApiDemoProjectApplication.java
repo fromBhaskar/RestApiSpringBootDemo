08a37967791e127f4fb761393986cd732288842c
Bhaskar Nishad <62388614+fromBhaskar@users.noreply.github.com>
2023-07-22 22:32:39 +0530

    Initial Commit

---
+++ RestApiDemoProject/src/main/java/com/fromBhaskar/RestApiDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.fromBhaskar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiDemoProjectApplication.class, args);
+	}
+
+}
